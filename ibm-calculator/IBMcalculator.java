6867142a98a9650a8e0c1c3575b4754a2fe4b459
Rafaela Bourdette <120579642+rafaelabou1999@users.noreply.github.com>
2023-07-07 14:26:50 -0300

    Create IBMcalculator.java

---
+++ ibm-calculator/IBMcalculator.java
@@ -0,0 +1,18 @@
+package com.example.studygui;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static java.awt.Color.white;
+import static java.awt.Font.MONOSPACED;
+
+public class IBMcalculator {
+
+    public static void main(String[] args) {
+
+        MyIBM ibm = new MyIBM();
+    }
+}
